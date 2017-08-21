db34c0e615dbdba3bf3d4d2a36116f64d47801c7
rahulpa <30974500+rahulpa@users.noreply.github.com>
2017-08-21 22:59:33 +0530

    save

--- src/generic/IAutoConst.java
+++ src/generic/IAutoConst.java
@@ -4,4 +4,5 @@ package generic;
 public interface IAutoConst {
 	String CHROME_KEY="webdriver.chrome.driver";
 	String CHROME_VALUE="./driver/chromedriver.exe";
+	String XLPATH="./data/input.xlsx";
 }
